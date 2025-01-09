42456d0cccb79f8be974357892f3efae77cde1cf
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2025-01-08 18:46:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,25 @@ import streamlit as st
 # Configuración de la página
 st.set_page_config(page_title="Podcast de Finanzas Juveniles", layout="wide")
 
+
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+
 # Sidebar: Descripción del podcast
 st.sidebar.title("Acerca del Podcast")
 st.sidebar.markdown("""
